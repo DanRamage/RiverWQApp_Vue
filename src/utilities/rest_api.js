fc5696d58555079b1e69e2b14893033b323a2744
DANRAMAGE <ChiefDan@gmail.com>
2026-05-18 08:18:49 -0400

    ADded observation data query api and then added 72 hour precip in alerts component.

--- src/utilities/rest_api.js
+++ src/utilities/rest_api.js
@@ -5,8 +5,8 @@ import axios from "axios";
 let BASE_API_URL = 'https://api.howsthebeach.org/api/v1/';
 //let BASE_API_URL = 'https://devapi.howsthebeach.org/api/v1/';
 let CAMERA_URL = "https://www.floridaapdata.org/beach/response_beach.php";
-let V2_API_URL = "http://127.0.0.1:5000/api/v2/";
-//let V2_API_URL = "https://devapi.howsthebeach.org/api/v2/";
+//let V2_API_URL = "http://127.0.0.1:5000/api/v2/";
+let V2_API_URL = "https://devapi.howsthebeach.org/api/v2/";
 
 export default {
     GetSiteData(site_name, site, startdate, enddate) {
